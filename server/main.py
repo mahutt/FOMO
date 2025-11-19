afe7c63a03d91b6b800ccea75718dcb0206bae99
mahutt <tommy.mahut@gmail.com>
2025-11-19 02:06:30 -0500

    Add slot db model / table and recurring sync with actual study room reservation statuses (webster)

--- server/main.py
+++ server/main.py
@@ -1,7 +1,16 @@
-from fastapi import FastAPI
+from datetime import datetime
+from typing import Annotated
+
+from fastapi import Depends, FastAPI, HTTPException, Query
 from pydantic import BaseModel
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+from fastapi_utils.tasks import repeat_every
+import httpx
+import asyncio
+from datetime import timedelta, date
 
 
+# Not peristed in DB
 class RoomStatus(BaseModel):
     room_id: str
     current_time: int
@@ -10,23 +19,114 @@ class RoomStatus(BaseModel):
     next_reservation_starts: int | None = None
 
 
+# Not peristed in DB
 class OccupancyLogEntry(BaseModel):
     room_id: str
     time: int
     occupied: bool
 
 
+# Persisted in DB
+class Slot(SQLModel, table=True):
+    itemId: int = Field(primary_key=True)
+    start: datetime = Field(primary_key=True)
+    end: datetime = Field(primary_key=True)
+    reserved: bool = Field(default=False)
+    occupied: bool = Field(default=False)
+
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+
+async def refresh_todays_slots(session: Session):
+    """Refresh slots for today"""
+    async with httpx.AsyncClient() as client:
+        try:
+            headers = {
+                "referer": "https://concordiauniversity.libcal.com/reserve/webster",
+                "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+            }
+            today = date.today()
+            data = {
+                "lid": "2161",
+                "gid": "5032",
+                "eid": "-1",
+                "seat": "0",
+                "seatId": "0",
+                "zone": "0",
+                "start": today.isoformat(),
+                "end": (today + timedelta(days=1)).isoformat(),
+                "pageIndex": "0",
+                "pageSize": "18",
+            }
+            response = await client.post(
+                "https://concordiauniversity.libcal.com/spaces/availability/grid",
+                headers=headers,
+                data=data,
+            )
+
+            slots = response.json().get("slots", [])
+            print(
+                f"Successfully fetched {len(slots)} slots using date {today} with status code: {response.status_code}"
+            )
+
+            slot_records = []
+            for slot in slots:
+                slot_record = Slot(
+                    itemId=slot["itemId"],
+                    start=datetime.fromisoformat(slot["start"]),
+                    end=datetime.fromisoformat(slot["end"]),
+                    reserved=("className" in slot),
+                    occupied=False,
+                )
+                session.merge(slot_record)
+            session.commit()
+            print("Database updated with latest slots.")
+
+        except Exception as e:
+            print("Error fetching slots:", e)
+
+
 app = FastAPI()
+
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
+
+@app.on_event("startup")
+@repeat_every(seconds=60 * 60 * 3)  # every 3 hours
+async def refresh_todays_slots_task():
+    with Session(engine) as session:
+        await refresh_todays_slots(session)
 
 
 @app.get("/")
 async def root():
     return {"message": "FOMO Server is running!"}
 
+
 @app.post("/sync/{room_id}")
 async def sync(room_id: str, data: list[OccupancyLogEntry]) -> RoomStatus:
-   print(data)
-   return RoomStatus(
+    print(data)
+    return RoomStatus(
         room_id=room_id,
         current_time=1625247600,
         currently_reserved=False,
